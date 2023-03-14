130dd19a2762f5e82c1b703a1453ffdf797ecd06
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-03-14 16:22:06 -0300

    feat 1 / im sick

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const readJsonData = require('./utils/readJsonData');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+const JSON_PATH = './src/talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const data = await readJsonData(JSON_PATH);
+
+  return res.status(200).json(data);
 });
 
 app.listen(PORT, () => {
