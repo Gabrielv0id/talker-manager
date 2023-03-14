0fd80241a526a231ffca6b53594052fd9ede2763
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-03-14 16:43:50 -0300

    feat 2

--- src/index.js
+++ src/index.js
@@ -16,9 +16,21 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (req, res) => {
   const data = await readJsonData(JSON_PATH);
 
-  return res.status(200).json(data);
+  return res.status(HTTP_OK_STATUS).json(data);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const data = await readJsonData(JSON_PATH);
+  const { id } = req.params;
+
+  const fillteredId = data.find((talker) => talker.id === Number(id));
+
+  if (!fillteredId) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(HTTP_OK_STATUS).json(fillteredId);
+});
 app.listen(PORT, () => {
   console.log('Online');
 });
